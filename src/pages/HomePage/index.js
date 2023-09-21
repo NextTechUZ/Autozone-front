85b3e18f66cf84da79fb0047488971adc8e98093
sherzodxon <yarmatxonovsherzodxon@gmailcom>
2023-09-21 11:36:30 +0500

    news section is created

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Advantages from '../../layouts/components/Advantages'
 import Services from '../../layouts/components/ServiceSection'
 import Info from '../../layouts/components/Info'
+import News from '../../layouts/components/News'
 
 function HomePage() {
   return (
@@ -9,6 +10,7 @@ function HomePage() {
     <Advantages/>
     <Services/>
     <Info/>
+    <News/>
     </>
   )
 }
